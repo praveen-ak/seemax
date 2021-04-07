69c2379b52a2a54af7f9062396740a4bf0294e9f
siva <siva.s@waioz.com>
2021-04-07 16:43:42 +0530

    width undefined - siva

--- js/util.js
+++ js/util.js
@@ -2,15 +2,12 @@ var util = (function() {
   function resize(canvases) {
     for (var i = 0; i < canvases.length; i++) {
       var canvas = canvases[i];
-      // canvas.width = document.documentElement.clientWidth;
-
-
-      canvas.width = window.innerWidth && document.documentElement.clientWidth ? 
-      Math.min(window.innerWidth, document.documentElement.clientWidth) : 
-      window.innerWidth || 
-      document.documentElement.clientWidth || 
-      document.getElementsByTagName('body')[0].clientWidth;
-
+      canvas.width = document.documentElement.clientWidth;
+      // canvas.width = window.innerWidth && document.documentElement.clientWidth ? 
+      // Math.min(window.innerWidth, document.documentElement.clientWidth) : 
+      // window.innerWidth || 
+      // document.documentElement.clientWidth || 
+      // document.getElementsByTagName('body')[0].clientWidth;
 
       canvas.height = document.documentElement.clientHeight / 2 * 1.3;
     }
